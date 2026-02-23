2fdabc694c79add33a8bc8455b31b29beaa45d2b
Snu Seam <siamahme766@gmail.com>
2026-02-23 12:29:18 +0600

    .

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,10 @@ import { NextFunction, Request, Response } from "express"
 const auth = () =>{
     return async (req:Request,res:Response,next:NextFunction)=>{
         const token = req.headers.authorization;
-        console.log(token);
+        // console.log(token);
+        if(!token){
+            throw new Error("you are not authorized");
+        }
         next();
     }
 }
