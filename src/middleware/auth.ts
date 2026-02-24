5aaa51a3de46f6d0fcf45da699b90e67534947d4
Snu Seam <siamahme766@gmail.com>
2026-02-24 12:44:03 +0600

    c

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,6 @@
+import  jwt  from 'jsonwebtoken';
 import { NextFunction, Request, Response } from "express"
+import config from '../config';
 
 const auth = () =>{
     return async (req:Request,res:Response,next:NextFunction)=>{
@@ -7,6 +9,8 @@ const auth = () =>{
         if(!token){
             throw new Error("you are not authorized");
         }
+        const decoded = jwt.verify(token, config.jwt_secret as string)
+        console.log(decoded);
         next();
     }
 }
